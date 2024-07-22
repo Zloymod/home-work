41edf7657c3b2a73d3f16247be251f2a8a07f3bf
Denis <165719269+Zloymod@users.noreply.github.com>
2024-07-22 20:05:42 +0300

    Add files via upload

---
+++ js-info.js
@@ -0,0 +1,7 @@
+alert("Брендан Эйх");
+alert(
+  "Синтаксис был создан за 10 дней, но разработка велась с 1992 по 1995 года"
+);
+alert("JavaScript появилс в 1995 году");
+alert("Javascript - это не Java");
+alert("ECMAScript");
